7ceee32cab11687d7530a8792e7a6fc428e9e2a0
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2026-06-06 20:37:12 +0300

    Полная переработка генератора input-событий
    Генерация событий создана заново с использованием генератора исходного кода Roslyn

---
+++ Scripts/Runtime/InputEventsWrapper/InputActionsWrapperBase.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine.InputSystem;
+
+namespace PostEnot.Toolkits.EventManagement.Input
+{
+    /// <summary>
+    /// Базовый класс для обёрток над <typeparamref name="TInputActions"/>, автоматизирующий
+    /// подписку на действия ввода и их отписку при освобождении.
+    /// </summary>
+    /// <typeparam name="TInputActions">
+    /// Тип, содержащий определения карт и действий ввода (обычно сгенерированный из .inputactions).
+    /// Должен реализовывать <see cref="IInputActionCollection2"/> и <see cref="IDisposable"/>.
+    /// </typeparam>
+    public abstract class InputActionsWrapperBase<TInputActions> where TInputActions : class, IInputActionCollection2, IDisposable
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="InputActionsWrapperBase{TInputActions}"/>,
+        /// сохраняет переданные зависимости и сразу вызывает <see cref="Register"/> для подписки на события ввода.
+        /// </summary>
+        /// <param name="inputActions">
+        /// Экземпляр <typeparamref name="TInputActions"/>, предоставляющий доступ к действиям ввода.
+        /// </param>
+        /// <param name="invoker">
+        /// Интерфейс для вызова событий в системе событий.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Выбрасывается, если <paramref name="inputActions"/> или <paramref name="invoker"/> равен <see langword="null"/>.
+        /// </exception>
+        public InputActionsWrapperBase(TInputActions inputActions, IEventInvoker invoker)
+        {
+            InputActions = inputActions ?? throw new ArgumentNullException(nameof(inputActions));
+            Invoker = invoker ?? throw new ArgumentNullException(nameof(invoker));
+            Register();
+        }
+
+        /// <summary>
+        /// Экземпляр <typeparamref name="TInputActions"/>, используемый для доступа к действиям ввода.
+        /// </summary>
+        public TInputActions InputActions { get; private set; }
+        /// <summary>
+        /// Интерфейс для вызова событий, через который отправляются сгенерированные структуры событий ввода.
+        /// </summary>
+        public IEventInvoker Invoker { get; private set; }
+        /// <summary>
+        /// <see langword="true"/>, если экземпляр уже был освобождён методом <see cref="Dispose"/>; иначе <see langword="false"/>.
+        /// </summary>
+        public bool IsDisposed { get; private set; }
+
+        /// <summary>
+        /// Вызывается при создании обёртки для подписки на все необходимые действия ввода.
+        /// </summary>
+        protected abstract void Register();
+
+        /// <summary>
+        /// Вызывается при освобождении обёртки для отписки от всех действий ввода.
+        /// </summary>
+        protected abstract void Unregister();
+
+        /// <summary>
+        /// Освобождает ресурсы, занятые обёрткой: отписывается от событий ввода и обнуляет
+        /// ссылки на <see cref="InputActions"/> и <see cref="Invoker"/>.
+        /// </summary>
+        public void Dispose()
+        {
+            if (IsDisposed)
+            {
+                return;
+            }
+            IsDisposed = true;
+            Unregister();
+            InputActions = null;
+            Invoker = null;
+        }
+    }
+}
---
+++ Scripts/Runtime/InputEventsWrapper/WrapInputEventsAttribute.cs
@@ -0,0 +1,31 @@
+﻿#nullable enable
+
+using System;
+
+namespace PostEnot.Toolkits.EventManagement.Input
+{
+    /// <summary>
+    /// Указывает, что для данного класса, реализующего <see cref="UnityEngine.InputSystem.IInputActionCollection2"/>,
+    /// необходимо сгенерировать класс-обёртку и класс событий ввода.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public sealed class WrapInputEventsAttribute : Attribute
+    {
+        /// <summary>
+        /// Пространство имён генерируемого класса событий. Если не задано, используется пространство имён исходного класса.
+        /// </summary>
+        public string? EventsNamespace { get; set; }
+        /// <summary>
+        /// Имя генерируемого класса событий. Если не задано, используется имя исходного класса с суффиксом "_Events".
+        /// </summary>
+        public string? EventsClassName { get; set; }
+        /// <summary>
+        /// Пространство имён генерируемого класса-обёртки. Если не задано, используется пространство имён исходного класса.
+        /// </summary>
+        public string? WrapperNamespace { get; set; }
+        /// <summary>
+        /// Имя генерируемого класса-обёртки. Если не задано, используется имя исходного класса с суффиксом "_Wrapper".
+        /// </summary>
+        public string? WrapperClassName { get; set; }
+    }
+}
